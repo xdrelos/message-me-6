5e9b73801ca65582284ed6e5442ec4a1acfd3c54
xdrelos <pierrejeancedric@gmail.com>
2021-04-11 14:06:43 +0200

    user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Cedric", password: "password")
+User.create(username: "Marc", password: "password")
+User.create(username: "Julie", password: "password")
+User.create(username: "Claire", password: "password")
+
+
