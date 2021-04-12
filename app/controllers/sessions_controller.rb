b99b5deae5503c0dc652a84bb5fdbe1d0b686a38
xdrelos <pierrejeancedric@gmail.com>
2021-04-12 09:59:49 +0200

    msg from ui

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully logged out"
+    flash[:success] = "You have successfully logged out."
     redirect_to login_path
   end
 
